9327955ea75268246c24c971cb2673e2a36c98f8
slikas <my.online0@gmail.com>
2021-02-17 22:51:14 +0700

    Cleanup autopager

--- Extensions/.libraries/voz.vn - .autopager.js
+++ Extensions/.libraries/voz.vn - .autopager.js
@@ -8,7 +8,8 @@ colorizeVerticalBorders(document.body)
 const selector_postBody = 'div.block-body.js-replyNewMessageContainer';
 const selector_threadList = 'div.structItemContainer-group.js-threadList';
 const selector_pageNav = isMobile() ? '.pageNavSimple' : '.pageNav  ';
-const selector_nextPage = isInThread()?selector_postBody:selector_threadList;
+const selector_nextBody = isInThread() ? selector_postBody : selector_threadList;
+const selector_nextPage = isMobile()? 'a.pageNavSimple-el.pageNavSimple-el--next':'a.pageNav-jump.pageNav-jump--next';
 const AUTOPAGER = {};
 AUTOPAGER.observer = {};
 AUTOPAGER.observer.callback = function (entries, observer) {
@@ -16,9 +17,17 @@ AUTOPAGER.observer.callback = function (entries, observer) {
 	loadNextPage();
 }
 AUTOPAGER.observer.main = new IntersectionObserver(AUTOPAGER.observer.callback);
-AUTOPAGER.observer.target = document.querySelector(isInThread()?'.block-outer.block-outer--after':'.p-breadcrumbs.p-breadcrumbs--bottom');
+AUTOPAGER.observer.target = document.querySelector(isInThread() ? '.block-outer.block-outer--after' : '.p-breadcrumbs.p-breadcrumbs--bottom');
 AUTOPAGER.observer.main.observe(AUTOPAGER.observer.target);
 
+function loadNextPage() {
+	//window.history.pushState(null, null, href_nextPage);
+	(async () => {
+		const response = await fetch(getHrefNextPage());
+		const textResponse = await response.text();
+		appendNextPage(textResponse, selector_nextBody);
+	})();
+}
 function appendNextPage(nextPageHtml, selector) {
 	let parser = new DOMParser();
 	let doc = parser.parseFromString(nextPageHtml, 'text/html');
@@ -33,22 +42,9 @@ function appendNextPage(nextPageHtml, selector) {
 
 	hideThreadsAndAddIgnoreButtons(nextPage);
 }
-function loadNextPage() {
-	//console.debug('%c#func: %s', styles.debug, getFuncName());
-	//window.history.pushState(null, null, href_nextPage);
-	(async () => {
-		const response = await fetch(getHrefNextPage());
-		const textResponse = await response.text();
-		appendNextPage(textResponse, selector_nextPage);
-	})();
-}
 function getHrefNextPage() {
-	const href_nextPage = isMobile() ?
-		document.querySelector('a.pageNavSimple-el.pageNavSimple-el--next').href :
-		document.querySelector('a.pageNav-jump.pageNav-jump--next').href;
-	return href_nextPage;
+	return document.querySelector(selector_nextPage).href;
 }
-
 if (isInSub()) {
 	try { document.querySelector('.structItemContainer-group--sticky').remove(); } catch { };
 }
