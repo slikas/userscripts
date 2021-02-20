ac97fa8c9d3cd7511df788080be7daa4a4b196c9
slikas <my.online0@gmail.com>
2021-02-20 12:39:14 +0700

    Replace if with switch for detecting hostname

--- Extensions/All sites - #stylizing.js
+++ Extensions/All sites - #stylizing.js
@@ -12,41 +12,43 @@
 // @downloadURL  https://raw.githubusercontent.com/slikas/userscripts/main/Extensions/All%20sites%20-%20%23stylizing.js
 // @updateURL    https://raw.githubusercontent.com/slikas/userscripts/main/Extensions/All%20sites%20-%20%23stylizing.js
 // ==/UserScript==
-//const CSS_COLOR_NAMES = ['red', 'blue', 'yellow', 'green', 'pink', 'orange', 'indigo', 'violet', 'navy', 'aqua', 'MAROON', 'purple'];
 const CSS_COLOR_NAMES = ['red', 'blue', 'yellow', 'green', 'pink', 'orange', 'indigo', 'violet', 'navy', 'aqua', 'MAROON', 'purple']
-const styles = {}
+const styles = {};
 styles.debug = 'color: blue';
 styles.log = 'color:green';
-
 colorizeVerticalBorders(document.body);
 alertNewVersion();
-if (window.location.hostname === "exhentai.org") {
-    const [navTop, navBot] = [document.querySelector('table.ptt'), document.querySelector('table.ptb')];
-    [navTop, navBot].forEach(nav => {
-        nav.style.width = '900px';
-        nav.style.height = '120px';
-        nav.style.fontSize = '30px';
-    })
+
+const hostname = window.location.hostname;
+switch (hostname) {
+    case "exhentai.org": {
+        const [navTop, navBot] = [document.querySelector('table.ptt'), document.querySelector('table.ptb')];
+        [navTop, navBot].forEach(nav => {
+            nav.style.width = '900px';
+            nav.style.height = '120px';
+            nav.style.fontSize = '30px';
+        })
+    }
 }
 function alertNewVersion() {
     const currentVersion = GM_getValue('scriptVersion') || '0';
-    const latestVersion = GM_info.script.version; 
-    if (latestVersion > currentVersion){
-        GM_setValue('scriptVersion',latestVersion);
-        alert('Script: ' + GM_info.script.name + 
+    const latestVersion = GM_info.script.version;
+    if (latestVersion > currentVersion) {
+        GM_setValue('scriptVersion', latestVersion);
+        alert('Script: ' + GM_info.script.name +
             '\nNew version: ' + latestVersion);
     }
 }
 
 function getFuncName() {
-	return getFuncName.caller.name
+    return getFuncName.caller.name
 }
 function isMobile() {
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		return true;
-	}
-	return false;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    }
+    return false;
 }
 function colorizeVerticalBorders(block) {
-	block.style.border = '4px dashed ' + CSS_COLOR_NAMES[Math.floor(Math.random() * CSS_COLOR_NAMES.length)];
+    block.style.border = '4px dashed ' + CSS_COLOR_NAMES[Math.floor(Math.random() * CSS_COLOR_NAMES.length)];
 }
