d63a2b2a3bc480d86bf00cb66b40d283f9cfc896
slikas <my.online0@gmail.com>
2021-02-25 12:58:33 +0700

    Add more template for hide-posts

--- Extensions/All sites - #hide-posts.js
+++ Extensions/All sites - #hide-posts.js
@@ -1,16 +1,38 @@
 // ==UserScript==
 // @name         All sites - #hide-posts
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @match        *://*/*
 // @exclude      https://voz.vn/f/*
 // @noframes
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @downloadURL  https://raw.githubusercontent.com/slikas/userscripts/main/Extensions/All%20sites%20-%20%23hide-posts.js
+// @updateURL    https://raw.githubusercontent.com/slikas/userscripts/main/Extensions/All%20sites%20-%20%23hide-posts.js
 // ==/UserScript==
 /* global shortenToWords */
-HIDE_THREADS = {}
-HIDE_THREADS.hrefJSON = 'https://climex.pythonanywhere.com/json/get-thread-ids';
-HIDE_THREADS.ids = [];
+const hostname = window.location.hostname;
+HIDE_POSTS = {}
+HIDE_POSTS.hrefJSON_host = 'https://climex.pythonanywhere.com/json/get-post-ids/';
+HIDE_POSTS.hrefJSON_full = new URL(hostname, HIDE_POSTS.hrefJSON_host).href;
+HIDE_POSTS.ids = [];
+
 hidePostsAndAddIgnoreButtons(document.body);
 function hidePostsAndAddIgnoreButtons(sourceHTML){
-    
+    addIgnoreButtons(sourceHTML);
+}
+function addIgnoreButtons(sourceHTML){
+
+}
+
+
+alertNewVersion();
+function alertNewVersion() {
+    const currentVersion = GM_getValue('scriptVersion') || '0';
+    const latestVersion = GM_info.script.version; 
+    if (latestVersion > currentVersion){
+        GM_setValue('scriptVersion',latestVersion);
+        alert('Script: ' + GM_info.script.name + 
+            '\nNew version: ' + latestVersion);
+    }
 }
